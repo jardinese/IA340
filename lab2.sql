446f3f7d050cc9eb23dab957d6c6d9dfb709b829
jardinese <jardineg@dukes.jmu.edu>
2024-09-04 15:37:22 -0400

    Create lab2.sql

---
+++ lab2.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS "Name" (
+	"tips" varchar(4) NOT NULL,
+	"name" varchar(100) NOT NULL UNIQUE,
+	PRIMARY KEY ("tips")
+);
+
+CREATE TABLE IF NOT EXISTS "Income" (
+	"tips" varchar(255) NOT NULL,
+	"income" bigint NOT NULL,
+	"year" bigint NOT NULL,
+	PRIMARY KEY ("tips", "year")
+);
+
+CREATE TABLE IF NOT EXISTS "Population" (
+	"tips" varchar(4) NOT NULL,
+	"pop" bigint NOT NULL,
+	"year" bigint NOT NULL,
+	PRIMARY KEY ("tips", "year")
+);
+
+
+ALTER TABLE "Income" ADD CONSTRAINT "Income_fk0" FOREIGN KEY ("tips") REFERENCES "Name"("tips");
+ALTER TABLE "Population" ADD CONSTRAINT "Population_fk0" FOREIGN KEY ("tips") REFERENCES "Name"("tips");
