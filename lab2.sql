03ec839c4c4263c99152dce42853ec067292c0dd
jardinese <jardineg@dukes.jmu.edu>
2024-09-04 15:54:30 -0400

    Update lab2.sql

--- lab2.sql
+++ lab2.sql
@@ -1,23 +1,23 @@
 CREATE TABLE IF NOT EXISTS "Name" (
 	"fips" varchar(4) NOT NULL,
 	"name" varchar(100) NOT NULL UNIQUE,
-	PRIMARY KEY ("tips")
+	PRIMARY KEY ("fips")
 );
 
 CREATE TABLE IF NOT EXISTS "Income" (
 	"fips" varchar(255) NOT NULL,
 	"income" bigint NOT NULL,
 	"year" bigint NOT NULL,
-	PRIMARY KEY ("tips", "year")
+	PRIMARY KEY ("fips", "year")
 );
 
 CREATE TABLE IF NOT EXISTS "Population" (
 	"fips" varchar(4) NOT NULL,
 	"pop" bigint NOT NULL,
 	"year" bigint NOT NULL,
-	PRIMARY KEY ("tips", "year")
+	PRIMARY KEY ("fips", "year")
 );
 
 
-ALTER TABLE "Income" ADD CONSTRAINT "Income_fk0" FOREIGN KEY ("tips") REFERENCES "Name"("tips");
-ALTER TABLE "Population" ADD CONSTRAINT "Population_fk0" FOREIGN KEY ("tips") REFERENCES "Name"("tips");
+ALTER TABLE "Income" ADD CONSTRAINT "Income_fk0" FOREIGN KEY ("fips") REFERENCES "Name"("fips");
+ALTER TABLE "Population" ADD CONSTRAINT "Population_fk0" FOREIGN KEY ("fips") REFERENCES "Name"("fips");
